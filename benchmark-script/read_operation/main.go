be06c6f37ef255b342ff082c86eed121e873e7e3
Prince Kumar <princer@google.com>
2023-08-24 01:23:55 +0000

    Added script to read the files

---
+++ benchmark-script/read_operation/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"strconv"
+	"sync"
+	"syscall"
+)
+
+var (
+	fDir          = flag.String("dir", "", "Directory file to be opened.")
+	fNumOfThreads = flag.Int("threads", 1, "Number of threads to read parallel")
+
+	fBlockSize = flag.Int("block_size", 256, "Block size in KB")
+
+	fileHandles []*os.File
+	wg          sync.WaitGroup
+
+	OneKB = 1024
+
+	fNumberOfRead = flag.Int("read_count", 1, "number of read iteration")
+)
+
+func openFile(index int) (err error) {
+	defer wg.Done()
+
+	fileName := path.Join(*fDir, "file_"+strconv.Itoa(index))
+	fileHandle, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY|syscall.O_DIRECT, 0600)
+	if err != nil {
+		err = fmt.Errorf("while opening file: %w", err)
+		return
+	}
+	fileHandles[index] = fileHandle
+	return
+}
+
+// Expect file is already opened, otherwise throws error
+func readAlreadyOpenedFile(index int) (err error) {
+	defer wg.Done()
+
+	for i := 0; i < *fNumberOfRead; i++ {
+		r := bufio.NewReader(fileHandles[index])
+		b := make([]byte, *fBlockSize*1024)
+
+		_, err = io.CopyBuffer(io.Discard, r, b)
+		if err != nil {
+			return fmt.Errorf("while reading and discarding content: %v", err)
+		}
+	}
+
+	return
+}
+
+func runReadFileOperations() (err error) {
+	if *fDir == "" {
+		err = fmt.Errorf("you must set --dir flag")
+		return
+	}
+
+	if *fNumOfThreads <= 0 {
+		err = fmt.Errorf("threads count not valid")
+		return
+	}
+
+	fileHandles = make([]*os.File, *fNumOfThreads)
+
+	for i := 0; i < *fNumOfThreads; i++ {
+		wg.Add(1)
+		err = openFile(i)
+		if err != nil {
+			err = fmt.Errorf("while opening file: %w", err)
+			return err
+		}
+	}
+
+	wg.Wait()
+
+	for i := 0; i < *fNumOfThreads; i++ {
+		wg.Add(1)
+		err = readAlreadyOpenedFile(i)
+		if err != nil {
+			err = fmt.Errorf("while reading file: %w", err)
+			return err
+		}
+	}
+
+	wg.Wait()
+
+	for i := 0; i < *fNumOfThreads; i++ {
+		if err = fileHandles[i].Close(); err != nil {
+			err = fmt.Errorf("while closing the fileHandle: %w", err)
+			return
+		}
+	}
+
+	return
+}
+
+func main() {
+	flag.Parse()
+
+	err := runReadFileOperations()
+	if err != nil {
+		fmt.Println(os.Stderr, err)
+		os.Exit(1)
+	}
+
+}
---
+++ benchmark-script/read_operation/read_operations.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+shopt -s expand_aliases
+
+threads=$1
+
+alias read_for_256KB="/home/princer_google_com/memory_work/custom-go-client-benchmark/benchmark-script/read_operation/read_operation --read_count 5000 --threads ${threads} --block_size 256 --dir \"/home/princer_google_com/gcs/reading/256KB\""
+
+alias read_for_1MB="/home/princer_google_com/memory_work/custom-go-client-benchmark/benchmark-script/read_operation/read_operation --read_count 1000 --threads ${threads} --block-size 1024 --dir \"/home/princer_google_com/gcs/reading/1MB\""
+
+alias read_for_100MB="/home/princer_google_com/memory_work/custom-go-client-benchmark/benchmark-script/read_operation/read_operation --read_count 100 --threads ${threads} --block-size 1024 --dir \"/home/princer_google_com/gcs/reading/100MB\""
+
+alias read_for_1GB="/home/princer_google_com/memory_work/custom-go-client-benchmark/benchmark-script/read_operation/read_operation --read_count 10 --threads ${threads} --block-size 1024 --dir \"/home/princer_google_com/gcs/reading/1GB\""
+
+# read for 256KB file
+echo "LoggerPrince: reading for 256KB with ${threads} threads"
+/home/princer_google_com/memory_work/gcsfuse/gcsfuse gcs-fuse-memory-profile-data ~/gcs
+ps -aux | grep "gcsfuse"
+read_for_256KB
+umount ~/gcs
+
+# read for 1MB file
+echo "LoggerPrince: reading for 1MB with ${threads} threads"
+/home/princer_google_com/memory_work/gcsfuse/gcsfuse gcs-fuse-memory-profile-data ~/gcs
+ps -aux | grep "gcsfuse"
+read_for_1MB
+umount ~/gcs
+
+# read for 100MB file
+echo "LoggerPrince: reading for 100MB with ${threads} threads"
+/home/princer_google_com/memory_work/gcsfuse/gcsfuse gcs-fuse-memory-profile-data ~/gcs
+ps -aux | grep "gcsfuse"
+read_for_100MB
+umount ~/gcs
+
+# read for 1GB file
+echo "LoggerPrince: reading for 1GB with ${threads} threads"
+/home/princer_google_com/memory_work/gcsfuse/gcsfuse gcs-fuse-memory-profile-data ~/gcs
+ps -aux | grep "gcsfuse"
+read_for_1GB
+umount ~/gcs
