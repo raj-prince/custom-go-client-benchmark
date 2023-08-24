70a33d5cdd3d12be6aac6dde088ca704644f500d
Prince Kumar <princer@google.com>
2023-08-24 03:43:18 +0000

    Increased read count for the read operations

--- benchmark-script/read_operation/read_operations.sh
+++ benchmark-script/read_operation/read_operations.sh
@@ -5,9 +5,9 @@ shopt -s expand_aliases
 
 threads=$1
 
-alias read_for_256KB="/home/princer_google_com/memory_work/custom-go-client-benchmark/benchmark-script/read_operation/read_operation --read-count 5000 --threads ${threads} --block-size 256 --dir \"/home/princer_google_com/gcs/reading/256KB\""
+alias read_for_256KB="/home/princer_google_com/memory_work/custom-go-client-benchmark/benchmark-script/read_operation/read_operation --read-count 50000 --threads ${threads} --block-size 256 --dir \"/home/princer_google_com/gcs/reading/256KB\""
 
-alias read_for_1MB="/home/princer_google_com/memory_work/custom-go-client-benchmark/benchmark-script/read_operation/read_operation --read-count 1000 --threads ${threads} --block-size 1024 --dir \"/home/princer_google_com/gcs/reading/1MB\""
+alias read_for_1MB="/home/princer_google_com/memory_work/custom-go-client-benchmark/benchmark-script/read_operation/read_operation --read-count 10000 --threads ${threads} --block-size 1024 --dir \"/home/princer_google_com/gcs/reading/1MB\""
 
 alias read_for_100MB="/home/princer_google_com/memory_work/custom-go-client-benchmark/benchmark-script/read_operation/read_operation --read-count 100 --threads ${threads} --block-size 1024 --dir \"/home/princer_google_com/gcs/reading/100MB\""
 
