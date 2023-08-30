d240ae53becdf15fb95fa8bc477ec0a36be626a6
Prince Kumar <princer@google.com>
2023-08-30 07:39:46 +0000

    updated write script

--- benchmark-script/write_operations/write_operations.sh
+++ benchmark-script/write_operations/write_operations.sh
@@ -3,10 +3,12 @@
 set -e
 shopt -s expand_aliases
 
-block_size=$1
-file_size=$2
+threads=$1
+block_size=$2
+file_size=$3
+write_count=$4
 
-alias write_file_cmd="/home/princer_google_com/memory_work/custom-go-client-benchmark/benchmark-script/write_operations/write_operations --block-size ${block_size} --file-size ${file_size}  --dir \"/home/princer_google_com/gcs/writing/\""
+alias write_file_cmd="/home/princer_google_com/memory_work/custom-go-client-benchmark/benchmark-script/write_operations/write_operations --threads ${threads} --block-size ${block_size} --file-size ${file_size} --write-count ${write_count} --dir \"/home/princer_google_com/gcs/writing/\""
 
 /home/princer_google_com/memory_work/gcsfuse/gcsfuse gcs-fuse-memory-profile-data ~/gcs
 ps -aux | grep "gcsfuse"
