b4fb8ae120d732eab316ef693c4741242034c40f
Prince Kumar <princer@google.com>
2023-09-03 12:49:06 +0000

    making asyn preemption off

--- benchmark-script/open_file/open_file_operation.sh
+++ benchmark-script/open_file/open_file_operation.sh
@@ -5,7 +5,7 @@ shopt -s expand_aliases
 
 file_count=$1
 
-alias open_file_cmd="/home/princer_google_com/memory_work/custom-go-client-benchmark/benchmark-script/open_file/open_file --open-files ${file_count} --dir \"/home/princer_google_com/gcs/listing/100K\""
+alias open_file_cmd="GODEBUG=asyncpreemptoff=1 /home/princer_google_com/memory_work/custom-go-client-benchmark/benchmark-script/open_file/open_file --open-files ${file_count} --dir \"/home/princer_google_com/gcs/listing/100K\""
 
 /home/princer_google_com/memory_work/gcsfuse/gcsfuse gcs-fuse-memory-profile-data ~/gcs
 ps -aux | grep "gcsfuse"
--- benchmark-script/write_operations/write_operations.sh
+++ benchmark-script/write_operations/write_operations.sh
@@ -8,7 +8,7 @@ block_size=$2
 file_size=$3
 write_count=$4
 
-alias write_file_cmd="/home/princer_google_com/memory_work/custom-go-client-benchmark/benchmark-script/write_operations/write_operations --threads ${threads} --block-size ${block_size} --file-size ${file_size} --write-count ${write_count} --dir \"/home/princer_google_com/gcs/writing/\""
+alias write_file_cmd="GODEBUG=asyncpreemptoff=1 /home/princer_google_com/memory_work/custom-go-client-benchmark/benchmark-script/write_operations/write_operations --threads ${threads} --block-size ${block_size} --file-size ${file_size} --write-count ${write_count} --dir \"/home/princer_google_com/gcs/writing/\""
 
 /home/princer_google_com/memory_work/gcsfuse/gcsfuse gcs-fuse-memory-profile-data ~/gcs
 ps -aux | grep "gcsfuse"
