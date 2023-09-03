a00878fb67d0a18b758fe0270e3aaddb95a7acca
Prince Kumar <princer@google.com>
2023-09-03 15:38:17 +0000

    Added with and without cache

--- benchmark-script/open_file/open_file_operation.sh
+++ benchmark-script/open_file/open_file_operation.sh
@@ -7,7 +7,14 @@ file_count=$1
 
 alias open_file_cmd="GODEBUG=asyncpreemptoff=1 /home/princer_google_com/memory_work/custom-go-client-benchmark/benchmark-script/open_file/open_file --open-files ${file_count} --dir \"/home/princer_google_com/gcs/listing/100K\""
 
-/home/princer_google_com/memory_work/gcsfuse/gcsfuse gcs-fuse-memory-profile-data ~/gcs
+echo "With cache"
+/home/princer_google_com/memory_work/gcsfuse/gcsfuse --type-cache-ttl 10m --stat-cache-ttl 10m --stat-cache-capacity 200000 gcs-fuse-memory-profile-data ~/gcs
+ps -aux | grep "gcsfuse"
+open_file_cmd
+umount ~/gcs
+
+echo "Without cache"
+/home/princer_google_com/memory_work/gcsfuse/gcsfuse --type-cache-ttl 0s --stat-cache-ttl 0s --stat-cache-capacity 0 gcs-fuse-memory-profile-data ~/gcs
 ps -aux | grep "gcsfuse"
 open_file_cmd
 umount ~/gcs
