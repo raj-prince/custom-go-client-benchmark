c9ff27f410b9bb16b7b805e89d429a27592a8fd6
Prince Kumar <princer@google.com>
2024-08-26 11:29:34 +0000

    Fixing the merge script

--- benchmark-script/warp_test/merge-csv-script.go
+++ benchmark-script/warp_test/merge-csv-script.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"math"
 	"os"
 	"path/filepath"
 	"sort"
@@ -17,10 +18,13 @@ type DataRow struct {
 
 func main() {
 	// Define the folder containing the CSV files
-	folder := "/usr/local/google/home/lankita/projects/test-bucket/metrics"
+	folder := "/usr/local/google/home/princer/csv/metrics"
 	
 	// Store all data rows from all files
 	var allDataRows []DataRow
+
+	cmnStartTime := int64(0)
+	cmnEndTime := int64(math.MaxInt64)
 
 	// Iterate over all CSV files in the folder
 	err := filepath.Walk(folder, func(path string, info os.FileInfo, err error) error {
@@ -29,6 +33,19 @@ func main() {
 		}
 		if filepath.Ext(path) == ".csv" {
 			dataRows, err := readCSV(path)
+			n := len(dataRows)
+			if n == 0 {
+				return fmt.Errorf("empty metrics-file")
+			}
+
+			// Get the time interval when all the nodes are active.
+			if cmnStartTime < dataRows[0].Timestamp {
+				cmnStartTime = dataRows[0].Timestamp
+			}
+			if cmnEndTime > dataRows[n - 1].Timestamp {
+				cmnEndTime = dataRows[n - 1].Timestamp
+			}
+
 			if err != nil {
 				return err
 			}
@@ -46,17 +63,25 @@ func main() {
 		return allDataRows[i].Timestamp < allDataRows[j].Timestamp
 	})
 
-	// Remove the first and last 50 rows
-	if len(allDataRows) > 100 {
-		allDataRows = allDataRows[50 : len(allDataRows)-50]
-	} else {
-		fmt.Println("Not enough data to filter first and last 5 rows")
-		return
+	// Filter out the data which is not part of common time interval.
+	var tmpFilteredRows []DataRow
+	var filteredCnt int
+	for _, data := range allDataRows {
+		if data.Timestamp < cmnStartTime || data.Timestamp > cmnEndTime {
+			filteredCnt++
+			continue
+		}
+		tmpFilteredRows = append(tmpFilteredRows, data)
 	}
+	allDataRows = tmpFilteredRows
+
+	fmt.Printf("total filtered: %v\n", filteredCnt)
+	printPercentiles(allDataRows)
 
 	// Write the filtered data to a new CSV file
-	outputFile := "/usr/local/google/home/lankita/projects/merged-and-filtered-data.csv"
+	outputFile := "/usr/local/google/home/princer/csv/output/output.csv"
 	err = writeCSV(outputFile, allDataRows)
+	fmt.Printf("Len: %d", len(allDataRows))
 	if err != nil {
 		fmt.Printf("Error writing CSV file: %v\n", err)
 		return
@@ -130,6 +155,46 @@ func writeCSV(path string, data []DataRow) error {
 	}
 
 	return nil
+}
+
+func printPercentiles(rows []DataRow) {
+	// Calculate averages
+	var totalLatency float64
+	for _, metric := range rows {
+		totalLatency += metric.ReadLatency
+	}
+	avgLatency := totalLatency / float64(len(rows))
+
+	// Calculate percentiles (e.g., 50th, 90th, 95th, 99th)
+	latencyValues := make([]float64, len(rows))
+	for i, metric := range rows {
+		latencyValues[i] = metric.ReadLatency
+	}
+
+	sort.Float64s(latencyValues)
+
+	fmt.Println("\n******* Metrics Summary: ReadLatency (s) *******")
+	fmt.Printf("Average Latency: %.2f\n", avgLatency)
+	fmt.Printf("p0: %.2f\n", percentileFloat64(latencyValues, 0))
+	fmt.Printf("p50: %.2f\n", percentileFloat64(latencyValues, 50))
+	fmt.Printf("p90: %.2f\n", percentileFloat64(latencyValues, 90))
+	fmt.Printf("p95: %.2f\n", percentileFloat64(latencyValues, 95))
+	fmt.Printf("p99: %.2f\n", percentileFloat64(latencyValues, 99))
+	fmt.Printf("p99.9: %.2f\n", percentileFloat64(latencyValues, 99.9))
+	fmt.Printf("p99.99: %.2f\n", percentileFloat64(latencyValues, 99.99))
+	fmt.Printf("p100: %.2f\n", percentileFloat64(latencyValues, 100))
+}
+
+func percentileFloat64(values []float64, p float32) float64 {
+	if p < 0 || p > 100 {
+		panic("Percentile must be between 1 and 100")
+	}
+
+	index := int((p / float32(100)) * float32(len(values)))
+	if index == len(values) {
+		index--
+	}
+	return values[index]
 }
 
 
