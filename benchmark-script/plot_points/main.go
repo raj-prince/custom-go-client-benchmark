c007175fbfbc0cc89e1fa9603ac98e69944b8829
Prince Kumar <princer@google.com>
2024-09-06 05:13:24 +0000

    Adding code to understand dynamice delay

---
+++ benchmark-script/plot_points/main.go
@@ -0,0 +1,220 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"image/color"
+	"math"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	"github.com/raj-prince/custom-go-client-benchmark/util"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+)
+
+func applySamples(p *plot.Plot, numSamples int, expectedValue float64, rnd *rand.Rand, d *util.Delay) {
+	var samplesOverThreshold int
+	for i := 0; i < numSamples; i++ {
+		randomDelay := time.Duration(-math.Log(rnd.Float64()) * expectedValue * float64(time.Second))
+		if randomDelay > d.Value() {
+			samplesOverThreshold++
+			d.Increase()
+		} else {
+			d.Decrease()
+		}
+		AddPoints(p, randomDelay.Seconds(), d.Value().Seconds())
+	}
+	fmt.Println("Over threshold: ", samplesOverThreshold)
+}
+
+// ConvertToXYs takes separate x and y slices and converts them into the correct plotter.XYs format
+func ConvertToXYs(xValues, yValues []float64) plotter.XYs {
+	if len(xValues) != len(yValues) {
+		panic("xValues and yValues must have the same length")
+	}
+
+	xys := make(plotter.XYs, len(xValues))
+	for i := range xValues {
+		xys[i].X = xValues[i]
+		xys[i].Y = yValues[i]
+	}
+	return xys
+}
+
+func actualSample(p *plot.Plot, dataRows []DataRow, d *util.Delay) {
+	totalCnt := 500
+	xValues := make([]float64, totalCnt)
+	yValues1 := make([]float64, totalCnt)
+	yValues2 := make([]float64, totalCnt)
+	var samplesOverThreshold int
+	for i, row := range dataRows {
+		if i >= totalCnt {
+			break
+		}
+
+		actualDelay := time.Duration(row.ReadLatency * float64(time.Second))
+		if actualDelay > d.Value() {
+			samplesOverThreshold++
+			d.Increase()
+		} else {
+			d.Decrease()
+		}
+		xValues[i] = float64(i)
+		yValues1[i] = d.Value().Seconds()
+		yValues2[i] = actualDelay.Seconds()
+
+		//AddPoints(p, actualDelay.Seconds(), d.Value().Seconds())
+	}
+
+	// Add line series for the first curve (sine)
+	line1, err := plotter.NewLine(ConvertToXYs(xValues, yValues1))
+	if err != nil {
+		panic(err)
+	}
+
+	line1.Color = color.RGBA{R: 255, A: 255} // Red color
+
+	// Add line series for the second curve (cosine)
+	line2, err := plotter.NewLine(ConvertToXYs(xValues, yValues2))
+	if err != nil {
+		panic(err)
+	}
+	line2.Color = color.RGBA{B: 255, A: 255} // Blue color
+	fmt.Println(len(dataRows))
+
+	// Add lines to the plot
+	p.Add(line1, line2)
+
+	fmt.Println("Over threshold: ", samplesOverThreshold)
+}
+
+func AddPoints(p *plot.Plot, x float64, y float64) {
+	// Create a slice of points (we'll have just one point)
+	pts := plotter.XYs{{X: x, Y: y}} // Adjust these coordinates as needed
+
+	// Add the points to the plot
+	scatter, err := plotter.NewScatter(pts)
+	if err != nil {
+		panic(err)
+	}
+	p.Add(scatter)
+}
+
+type DataRow struct {
+	Timestamp   int64
+	ReadLatency float64
+	Throughput  float64
+}
+
+func GetDataRows(folder string) ([]DataRow, error) {
+	// Store all data rows from all files
+	var allDataRows []DataRow
+
+	cmnStartTime := int64(0)
+	cmnEndTime := int64(math.MaxInt64)
+
+	// Iterate over all CSV files in the folder
+	err := filepath.Walk(folder, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if filepath.Ext(path) == ".csv" {
+			dataRows, err := readCSV(path)
+			n := len(dataRows)
+			if n == 0 {
+				return fmt.Errorf("empty metrics-file")
+			}
+
+			// Get the time interval when all the nodes are active.
+			if cmnStartTime < dataRows[0].Timestamp {
+				cmnStartTime = dataRows[0].Timestamp
+			}
+			if cmnEndTime > dataRows[n-1].Timestamp {
+				cmnEndTime = dataRows[n-1].Timestamp
+			}
+
+			if err != nil {
+				return err
+			}
+			allDataRows = append(allDataRows, dataRows...)
+		}
+		return nil
+	})
+
+	fmt.Println(cmnStartTime)
+	fmt.Println(cmnEndTime)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading CSV files: %v\n", err)
+	}
+
+	return allDataRows, nil
+}
+
+// readCSV reads a CSV file and returns a slice of DataRow
+func readCSV(path string) ([]DataRow, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	// Skip the header row
+	_, err = reader.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	var dataRows []DataRow
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			break
+		}
+		timestamp, _ := strconv.ParseInt(record[0], 10, 64)
+		readLatency, _ := strconv.ParseFloat(record[1], 64)
+		throughput, _ := strconv.ParseFloat(record[2], 64)
+
+		dataRows = append(dataRows, DataRow{
+			Timestamp:   timestamp,
+			ReadLatency: readLatency,
+			Throughput:  throughput,
+		})
+	}
+
+	return dataRows, nil
+}
+
+func main() {
+	// Create a new plot
+	p := plot.New()
+
+	dataRows, err := GetDataRows("/usr/local/google/home/princer/csv/metrics")
+	if err != nil {
+		fmt.Println("Error while fetching datarows")
+		return
+	}
+
+	// Set the title and axis labels
+	p.Title.Text = "Single Point Plot"
+	p.X.Label.Text = "X"
+	p.Y.Label.Text = "Y"
+
+	delay, err := util.NewDelay(0.99, 100, 500*time.Millisecond, 1*time.Hour)
+	if err != nil {
+		panic(err)
+	}
+
+	//applySamples(p, 1000, 0.05, rand.New(rand.NewSource(1)), delay)
+	actualSample(p, dataRows, delay)
+
+	// Save the plot as a PNG image
+	if err := p.Save(20*vg.Inch, 20*vg.Inch, "point_plot.png"); err != nil {
+		panic(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	cloud.google.com/go/storage v1.40.0
 	contrib.go.opencensus.io/exporter/stackdriver v0.13.14
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace v1.20.0
+	github.com/google/go-cmp v0.6.0
 	github.com/googleapis/gax-go/v2 v2.12.3
 	github.com/jacobsa/gcloud v0.0.0-20230425120041-5ed2958cdfee
 	go.opencensus.io v0.24.0
@@ -16,6 +17,7 @@ require (
 	go.opentelemetry.io/otel/trace v1.24.0
 	golang.org/x/oauth2 v0.18.0
 	golang.org/x/sync v0.6.0
+	gonum.org/v1/plot v0.14.0
 	google.golang.org/api v0.170.0
 	google.golang.org/grpc v1.62.1
 )
@@ -27,9 +29,12 @@ require (
 	cloud.google.com/go/iam v1.1.7 // indirect
 	cloud.google.com/go/monitoring v1.18.0 // indirect
 	cloud.google.com/go/trace v1.10.5 // indirect
+	git.sr.ht/~sbinet/gg v0.5.0 // indirect
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/detectors/gcp v1.19.1 // indirect
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/internal/resourcemapping v0.44.0 // indirect
+	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
 	github.com/aws/aws-sdk-go v1.43.31 // indirect
+	github.com/campoy/embedmd v1.0.0 // indirect
 	github.com/census-instrumentation/opencensus-proto v0.4.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/cncf/udpa/go v0.0.0-20220112060539-c52dc94e7fbe // indirect
@@ -37,8 +42,12 @@ require (
 	github.com/envoyproxy/go-control-plane v0.12.0 // indirect
 	github.com/envoyproxy/protoc-gen-validate v1.0.4 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-fonts/liberation v0.3.1 // indirect
+	github.com/go-latex/latex v0.0.0-20230307184459-12ec69307ad9 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-pdf/fpdf v0.8.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
@@ -48,12 +57,14 @@ require (
 	github.com/jacobsa/oglemock v0.0.0-20150831005832-e94d794d06ff // indirect
 	github.com/jacobsa/reqtrace v0.0.0-20150505043853-245c9e0234cb // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/prometheus v0.35.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.49.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
 	go.opentelemetry.io/otel/metric v1.24.0 // indirect
 	go.opentelemetry.io/otel/sdk/metric v1.19.0 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/image v0.11.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
