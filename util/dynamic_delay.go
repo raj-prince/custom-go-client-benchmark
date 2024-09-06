d00077a50882be29b4cdd1083ec22dd5cf57784c
Prince Kumar <princer@google.com>
2024-09-06 02:43:11 +0000

    Adding dynamic delay

---
+++ util/dynamic_delay.go
@@ -0,0 +1,103 @@
+package util
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+// Package dynamicdelay calculates the delay at a fixed percentile, based on
+// delay samples.
+//
+//
+// Delay is not goroutine-safe.
+type Delay struct {
+	increaseFactor float64
+	decreaseFactor float64
+	minDelay       time.Duration
+	maxDelay       time.Duration
+	value          time.Duration
+}
+
+// NewDelay returns a Delay.
+//
+// targetPercentile is the desired percentile to be computed. For example, a
+// targetPercentile of 0.99 computes the delay at the 99th percentile. Must be
+// in the range [0, 1].
+//
+// increaseRate (must be > 0) determines how many Increase calls it takes for
+// Value to double.
+//
+// Decrease can never lower the delay past minDelay, Increase can never raise
+// the delay past maxDelay.
+func NewDelay(targetPercentile float64, increaseRate float64, minDelay, maxDelay time.Duration) (*Delay, error) {
+	if targetPercentile < 0 || targetPercentile > 1 {
+		return nil, fmt.Errorf("invalid targetPercentile (%v): must be within [0, 1]", targetPercentile)
+	}
+	if increaseRate <= 0 {
+		return nil, fmt.Errorf("invalid increaseRate (%v): must be > 0", increaseRate)
+	}
+	if minDelay >= maxDelay {
+		return nil, fmt.Errorf("invalid minDelay (%v) and maxDelay (%v) combination: minDelay must be smaller than maxDelay", minDelay, maxDelay)
+	}
+	// See http://google3/net/rpc/contrib/hedged_call/dynamic_delay.h?l=35&rcl=194811090
+	increaseFactor := math.Exp(math.Log(2) / increaseRate)
+	if increaseFactor < 1.001 {
+		increaseFactor = 1.001
+	}
+	decreaseFactor := math.Exp(-math.Log(increaseFactor) * (1 - targetPercentile) / targetPercentile)
+	if decreaseFactor > 0.9999 {
+		decreaseFactor = 0.9999
+	}
+
+	return &Delay{
+		increaseFactor: increaseFactor,
+		decreaseFactor: decreaseFactor,
+		minDelay:       minDelay,
+		maxDelay:       maxDelay,
+		value:          minDelay,
+	}, nil
+}
+
+// increase notes that the RPC took longer than the delay returned by Value.
+func (d *Delay) Increase() {
+	v := time.Duration(float64(d.value) * d.increaseFactor)
+	if v > d.maxDelay {
+		d.value = d.maxDelay
+	} else {
+		d.value = v
+	}
+}
+
+// decrease notes that the RPC completed before the delay returned by Value.
+func (d *Delay) Decrease() {
+	v := time.Duration(float64(d.value) * d.decreaseFactor)
+	if v < d.minDelay {
+		d.value = d.minDelay
+	} else {
+		d.value = v
+	}
+}
+
+// Update notes that the RPC either took longer than the delay or completed
+// before the delay, depending on the specified latency.
+func (d *Delay) Update(latency time.Duration) {
+	if latency > d.value {
+		d.Increase()
+	} else {
+		d.Decrease()
+	}
+}
+
+// Value returns the desired delay to wait before hedging the RPC call.
+func (d *Delay) Value() time.Duration {
+	return d.value
+}
+
+func (d *Delay) PrintDelay() {
+	fmt.Println("IncreaseFactor: ", d.increaseFactor)
+	fmt.Println("DecreaseFactor: ", d.decreaseFactor)
+	fmt.Println("MinDelay: ", d.minDelay)
+	fmt.Println("MaxDelay: ", d.maxDelay)
+	fmt.Println("Value: ", d.value)
+}
---
+++ util/dynamic_delay_test.go
@@ -0,0 +1,137 @@
+package util
+
+import (
+	"fmt"
+	"math"
+"math/rand"
+"testing"
+"time"
+
+"github.com/google/go-cmp/cmp"
+"github.com/google/go-cmp/cmp/cmpopts"
+)
+
+func applySamples(numSamples int, expectedValue float64, rnd *rand.Rand, d *Delay) int {
+	var samplesOverThreshold int
+	for i := 0; i < numSamples; i++ {
+		randomDelay := time.Duration(-math.Log(rnd.Float64()) * expectedValue * float64(time.Second))
+		if randomDelay > d.Value() {
+			samplesOverThreshold++
+			d.Increase()
+		} else {
+			d.Decrease()
+		}
+	}
+	return samplesOverThreshold
+}
+
+func applySamplesWithUpdate(numSamples int, expectedValue float64, rnd *rand.Rand, d *Delay) {
+	for i := 0; i < numSamples; i++ {
+		randomDelay := time.Duration(-math.Log(rnd.Float64()) * expectedValue * float64(time.Second))
+		//randomDelay := time.Duration(rand.Float64() * 1000000000)
+		fmt.Println("Added value: ", randomDelay)
+		d.Update(randomDelay)
+		fmt.Println("Timeout: ", d.Value())
+	}
+}
+
+func TestNewDelay(t *testing.T) {
+	d, err := NewDelay(1-0.01, 15, 1*time.Millisecond, 1*time.Hour)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// The following values were printed from
+	// http://google3/net/rpc/contrib/hedged_call/dynamic_delay.h?l=47&rcl=194811090
+	want := &Delay{
+		increaseFactor: 1.047294,
+		decreaseFactor: 0.999533,
+		minDelay:       1 * time.Millisecond,
+		maxDelay:       1 * time.Hour,
+		value:          1 * time.Millisecond,
+	}
+	if diff := cmp.Diff(d, want, cmp.AllowUnexported(Delay{}), cmpopts.EquateApprox(0, 0.000001)); diff != "" {
+		t.Fatalf("unexpected diff (-got +want):\n%s", diff)
+	}
+}
+
+func TestConvergence99(t *testing.T) {
+	// d should converge to the 99-percentile value.
+	d, err := NewDelay(1-0.01, 15, 500*time.Millisecond, 10*time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rnd := rand.New(rand.NewSource(1))
+
+	// Warm up.
+	applySamplesWithUpdate(1000, 0.005, rnd, d)
+
+	// We would end up sending hedged calls at ~1% (between 0.2% and 5%).
+	{
+		samplesOverThreshold := applySamples(1000, 0.005, rnd, d)
+		if samplesOverThreshold < (1000 * 0.002) {
+			t.Errorf("samplesOverThreshold = %d < 1000*0.002", samplesOverThreshold)
+		}
+		if samplesOverThreshold > (1000 * 0.05) {
+			t.Errorf("samplesOverThreshold = %d > 1000*0.05", samplesOverThreshold)
+		}
+	}
+
+	// Apply samples from a different distribution.
+	applySamplesWithUpdate(1000, 1, rnd, d)
+
+	// delay.value should have now converged to the new distribution.
+	{
+		samplesOverThreshold := applySamples(1000, 1, rnd, d)
+		if samplesOverThreshold < (1000 * 0.002) {
+			t.Errorf("samplesOverThreshold = %d < 1000*0.002", samplesOverThreshold)
+		}
+		if samplesOverThreshold > (1000 * 0.05) {
+			t.Errorf("samplesOverThreshold = %d > 1000*0.05", samplesOverThreshold)
+		}
+	}
+}
+
+func TestConvergence90(t *testing.T) {
+	// d should converge to the 90-percentile value.
+	d, err := NewDelay(1-0.1, 15, 1*time.Millisecond, 1*time.Hour)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rnd := rand.New(rand.NewSource(1))
+
+	// Warm up.
+	applySamplesWithUpdate(1000, 0.005, rnd, d)
+
+	// We would end up sending hedged calls at ~10% (between 5% and 20%).
+	{
+		samplesOverThreshold := applySamples(1000, 0.005, rnd, d)
+		if samplesOverThreshold < (1000 * 0.05) {
+			t.Errorf("samplesOverThreshold = %d < 1000*0.05", samplesOverThreshold)
+		}
+		if samplesOverThreshold > (1000 * 0.2) {
+			t.Errorf("samplesOverThreshold = %d > 1000*0.2", samplesOverThreshold)
+		}
+	}
+}
+
+func TestOverflow(t *testing.T) {
+	d, err := NewDelay(1-0.1, 15, 1*time.Millisecond, 1*time.Hour)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	n := 10000
+	for i := 0; i < n; i++ {
+		d.Increase()
+	}
+	t.Log(d.Value())
+	for i := 0; i < 100*n; i++ {
+		d.Decrease()
+	}
+	if got, want := d.Value(), 1*time.Millisecond; got != want {
+		t.Fatalf("unexpected d.Value: got %v, want %v", got, want)
+	}
+}
+
