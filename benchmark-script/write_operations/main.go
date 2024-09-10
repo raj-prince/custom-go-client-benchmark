9a20c9bb3d1a2765f13cb76978cab8b0ca9e12e4
vadlakondaswetha <swethv@google.com>
2024-09-10 06:05:30 +0000

    adding prefix

--- benchmark-script/write_operations/main.go
+++ benchmark-script/write_operations/main.go
@@ -14,6 +14,7 @@ import (
 )
 
 var (
+	fPrefix       = flag.String("prefix", "", "prefix for each file.")
 	fDir          = flag.String("dir", "", "Directory file to be opened.")
 	fNumOfThreads = flag.Int("threads", 1, "Number of threads to read parallel")
 
@@ -38,7 +39,7 @@ func init() {
 }
 
 func openFile(fileSuffix string) (fileHandle *os.File, err error) {
-	fileName := path.Join(*fDir, "file_"+fileSuffix)
+	fileName := path.Join(*fDir, *fPrefix+"_file_"+fileSuffix)
 	fileHandle, err = os.OpenFile(fileName, os.O_CREATE|os.O_RDWR|os.O_TRUNC|syscall.O_DIRECT, 0644)
 	if err != nil {
 		err = fmt.Errorf("while opening file: %w", err)
@@ -50,6 +51,9 @@ func openFile(fileSuffix string) (fileHandle *os.File, err error) {
 
 // Expect file is already opened, otherwise throws error
 func overWriteAlreadyOpenedFile(index int) (err error) {
+	b := make([]byte, *fBlockSize*OneKB)
+	_, err = rand.Read(b)
+
 	for cnt := 0; cnt < *fNumOfWrite; cnt++ {
 		writeStart := time.Now()
 		fileName := strconv.Itoa(index) + "__" + strconv.Itoa(cnt)
@@ -59,9 +63,6 @@ func overWriteAlreadyOpenedFile(index int) (err error) {
 		}
 
 		for i := 0; i < (*fFileSize / *fBlockSize); i++ {
-			b := make([]byte, *fBlockSize*OneKB)
-
-			_, err = rand.Read(b)
 			if err != nil {
 				return fmt.Errorf("while generating random bytest: %v", err)
 			}
@@ -111,11 +112,8 @@ func runReadFileOperations() (err error) {
 
 	err = eG.Wait()
 
-	if err == nil {
-		fmt.Println("write benchmark completed successfully!")
-		fmt.Println("Waiting for 1 minutes")
-
-		time.Sleep(1 * time.Minute)
+	if err != nil {
+		return err
 	}
 
 	return
