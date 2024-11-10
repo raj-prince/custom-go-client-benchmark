7de2a528b84d3c07ed185a34f25b77631ec059a6
raj-prince <princer@google.com>
2024-11-10 15:33:04 +0000

    chore: supporting range-read in warp test

--- benchmark-script/read_operation/run_warp_test.sh
+++ benchmark-script/read_operation/run_warp_test.sh
@@ -3,4 +3,9 @@
 set -e
 shopt -s expand_aliases
 
+echo "Running sequential read test..."
 time go run . --threads 64 --read-count 1 --file-size-mb 64 --dir /home/princer_google_com/warp-test/gcs/64M/ --file-prefix "experiment."
+
+
+echo "Running random read test..."
+time go run . --threads 64 --read-count 1 --file-size-mb 64 --dir /home/princer_google_com/warp-test/gcs/64M/ --file-prefix "experiment." --read  "randread" --block-size-kb 8192
