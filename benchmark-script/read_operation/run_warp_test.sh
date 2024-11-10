0522e6fa09184ebbe8739aef190e57fea6a99734
raj-prince <princer@google.com>
2024-11-10 17:07:44 +0000

    chore: adding support for 5G file

--- benchmark-script/read_operation/run_warp_test.sh
+++ benchmark-script/read_operation/run_warp_test.sh
@@ -5,7 +5,7 @@ shopt -s expand_aliases
 
 echo "Running sequential read test..."
 time go run . --threads 64 --read-count 1 --file-size-mb 64 --dir /home/princer_google_com/warp-test/gcs/64M/ --file-prefix "experiment."
-
+time go run . --threads 64 --read-count 1 --file-size-mb 1024 --dir /home/princer_google_com/warp-test/gcs/64M/ --file-prefix "experiment."
 
 echo "Running random read test..."
-time go run . --threads 64 --read-count 1 --file-size-mb 64 --dir /home/princer_google_com/warp-test/gcs/64M/ --file-prefix "experiment." --read  "randread" --block-size-kb 8192
+time go run . --threads 64 --read-count 1 --file-size-mb 5120 --dir /home/princer_google_com/warp-test/gcs/5G/ --file-prefix "experiment." --read  "randread" --block-size-kb 8192
