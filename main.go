887256358e30b6c3f85da86193da9a5ad16b82f7
vadlakondaswetha <swethv@google.com>
2025-02-27 06:31:30 +0000

    write via gcsfuse

--- main.go
+++ main.go
@@ -347,7 +347,7 @@ func main() {
 	for i := 0; i < *numOfWorker; i++ {
 		idx := i
 		eG.Go(func() error {
-			err = WriteObject(ctx, idx, bucketHandle)
+			err = WriteViaGCSFuse(ctx, idx, bucketHandle)
 			if err != nil {
 				err = fmt.Errorf("while reading object %v: %w", *objectNamePrefix+strconv.Itoa(idx)+*objectNameSuffix, err)
 				return err
